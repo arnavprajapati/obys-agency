1b921a6d000660b0889df50e6df1a57e2674ae87
Arnav <arnavprajapati3101@gmail.com>
2025-05-16 13:08:14 +0530

    cursor and magnet effect

--- script.js
+++ script.js
@@ -1,47 +1,65 @@
-var tl = gsap.timeline();
+function loadingAnimation() {
 
-tl.from(".line h1",{
-    y:150,
-    stagger:0.25,
-    duration:0.6,
-    delay:0.5
-})
+    var tl = gsap.timeline();
 
-tl.from('#line1-part1',{
-    opacity: 0,
-    onStart:function(){
-        var h5timer = document.querySelector("#line1-part1 h5")
-        var grow = 0
-        setInterval(() => {
-            if(grow<100){
-                h5timer.innerHTML = grow++
-            }else{
-                h5timer.innerHTML = grow
-            }
-        }, 27)
-    }
-})
+    tl.from(".line h1",{
+        y:150,
+        stagger:0.25,
+        duration:0.6,
+        delay:0.5
+    })
 
-tl.to('.line h2, #wait',{
-    animationName: "anime",
-    opacity: 1
-})
+    tl.from('#line1-part1',{
+        opacity: 0,
+        onStart:function(){
+            var h5timer = document.querySelector("#line1-part1 h5")
+            var grow = 0
+            setInterval(() => {
+                if(grow<100){
+                    h5timer.innerHTML = grow++
+                }else{
+                    h5timer.innerHTML = grow
+                }
+            }, 27)
+        }
+    })
+
+    tl.to('.line h2, #wait',{
+        animationName: "anime",
+        opacity: 1
+    })
 
 
-tl.to('#loader',{
-    opacity: 0,
-    duration: 0.4,
-    delay: 4
-})
+    tl.to('#loader',{
+        opacity: 0,
+        duration: 0.4,
+        delay: 4
+    })
 
-tl.from('#page1',{
-    delay:0.2,
-    y:1600,
-    opacity:0,
-    duration:0.5,
-    ease: Power4
-})
+    tl.from('#page1',{
+        delay:0.2,
+        y:1600,
+        opacity:0,
+        duration:0.5,
+        ease: Power4
+    })
 
-tl.to("#loader",{
-    display:"none",
-})
+    tl.to("#loader",{
+        display:"none",
+    })
+}
+
+
+function cursorAnimation(){
+    document.addEventListener("mousemove",(dets) => {
+        gsap.to('#crsr',{
+            left: dets.x,
+            top: dets.y,
+        })
+    })
+
+    Shery.makeMagnet("#nav-part2 h4",{});
+}
+
+loadingAnimation()
+cursorAnimation()
